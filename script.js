9f70d23c627cd551724b1c592ad0baad1857c55d
mleers <mleers4@gmail.com>
2019-10-25 14:19:57 -0700

    Updated index.html, script.js and added style.css to create ui for rps game

--- script.js
+++ script.js
@@ -1,71 +1,124 @@
+let playerSelection;
 let playerScore = 0;
-    let computerScore = 0;
+let computerScore = 0;
+let gameWinner = '';
+let gameOver = false;
+const gameButtons = document.querySelectorAll('button');
 
-    let computerPlay = () => {
-        moves = ["rock", "paper", "scissors"];
-        computerSelection = moves[Math.floor(Math.random()*moves.length)];
-        return computerSelection;
-    }
+const computerPlay = () => {
+	let randomPick = Math.floor(Math.random() * 3); // random assignment to play options
+	return (randomPick === 0) ? 'Rock' : (randomPick === 1) ? 'Paper' : 'Scissors';
+}
+
+gameButtons.forEach((gameButton) => {
+	gameButton.addEventListener('click', function(event) {
+		playerSelection = event.target.textContent;
+		if (gameOver === true) return; // only play again if previous round finished
+		playRound(playerSelection);
+	});
+});
+
+function playRound(playerSelection) {
+	let computerSelection = computerPlay(); // run once each round
+	let bothPlays = playerSelection + ' vs. ' + computerSelection;
+	let endRoundMessage;  // win or loss message per round
+
+	if (playerSelection === computerSelection) {
+		bothPlays = 'tie';
+	}
+
+	// compare for each case and for tie, cleaner than if statements
+	switch (bothPlays) {
+		case 'Rock vs. Scissors' : 
+		case 'Paper vs. Rock' : 
+		case 'Scissors vs. Paper' :
+        endRoundMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+		break;
+
+		case 'tie' : 
+        endRoundMessage = 'Tie, replay to continue';
+		break;
+
+		default : 
+        endRoundMessage = `You lost! ${computerSelection} beats ${playerSelection}`;
+		break;
+	}
+	view.displayResults(endRoundMessage);
+	score(endRoundMessage);
+}
+
+const score = (roundResult) => {
+	if (roundResult.startsWith("You win")) {
+		playerScore++;
+	} else if (roundResult.startsWith("You lost")) {
+		computerScore++;
+	} 
+	view.displayScore(playerScore, computerScore);
+
+	if (playerScore === 5 || computerScore === 5) {
+		determineWinner();
+		gameOver = true;
+	}
+};
+
+const determineWinner = () => {
+	if (playerScore === 5) {
+		gameWinner = 'You are the winner!';
+		view.displayWinner(gameWinner);
+	} else if (computerScore === 5) {
+		gameWinner = 'The computer wins the game!';
+		view.displayWinner(gameWinner);
+	}
+	view.displayPlayAgain();
+};
 
 
-    let playRound = (playerSelection, computerSelection) => {
-        
+const view = {
+	displayResults: function(endRoundmessage) {
+		const resultsDiv = document.querySelector('#results');
+		resultsDiv.textContent = endRoundmessage;
+	},
+	displayScore: function(playerScore, computerScore) {
+		const playerScoreTd = document.querySelector('#your-score');
+		const computerScoreTd = document.querySelector('#computer-score');
+		playerScoreTd.textContent = playerScore;
+		computerScoreTd.textContent = computerScore;
+	},
+	displayWinner: function(gameWinner) {
+		const scoreBoard = document.querySelector('#scoreboard');
+		const winnerDiv = document.createElement('div');
+		winnerDiv.setAttribute('id', 'winner');
+		winnerDiv.textContent = gameWinner;
+		scoreBoard.appendChild(winnerDiv);
+	},
+	displayPlayAgain: function() {
+		const playAgainButton = document.createElement('button');
+		const scoreBoard = document.querySelector('#scoreboard');
+		playAgainButton.setAttribute('id', 'play-again-button');
+		playAgainButton.textContent = 'Play Again';
+		scoreBoard.appendChild(playAgainButton);
+		playAgainButton.addEventListener('click', restartGame);
+	},
+	removePlayAgain: function() {
+		const playAgainButton = document.querySelector('#play-again-button');
+		const scoreBoard = document.querySelector('#scoreboard');
+		scoreBoard.removeChild(playAgainButton);
+	},
+	removeWinner: function() {
+		const scoreBoard = document.querySelector('#scoreboard');
+		const winnerDiv = document.querySelector('#winner');
+		scoreBoard.removeChild(winnerDiv);
+	}
+};
 
-        if (playerSelection.toLowerCase() == "rock" &&  computerSelection == "scissors") {
-                playerScore += 1;
-                return `You played ${playerSelection.toLowerCase()} and beat Computer's ${computerSelection}! Score: ${playerScore} to ${computerScore}`;   
-         }
+const restartGame = () => {
+	gameOver = false;
+	playerScore = 0;
+	computerScore = 0;
+	gameWinner = '';
 
-         else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
-                playerScore += 1;
-                return `You played ${playerSelection.toLowerCase()} and beat Computer's ${computerSelection}! Score: ${playerScore} to ${computerScore}`;
-                
-        }
-
-        else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
-                playerScore += 1;
-                return `You played ${playerSelection.toLowerCase()} and beat Computer's ${computerSelection}! Score: ${playerScore} to ${computerScore}`;
-                
-        }
-
-        else if (computerSelection == "rock" &&  playerSelection.toLowerCase() == "scissors") {
-                computerScore += 1;
-                return `You played ${playerSelection.toLowerCase()} and lost to Computer's ${computerSelection}! Score: ${playerScore} to ${computerScore}`;   
-         }
-
-         else if (computerSelection == "paper" && playerSelection.toLowerCase() == "rock") {
-                computerScore += 1;
-                return `You played ${playerSelection.toLowerCase()} and lost to Computer's ${computerSelection}! Score: ${playerScore} to ${computerScore}`;
-                
-        }
-
-        else if (computerSelection == "scissors" && playerSelection.toLowerCase() == "paper") {
-                computerScore += 1;
-                return `You played ${playerSelection.toLowerCase()} and lost to Computer's ${computerSelection}! Score: ${playerScore} to ${computerScore}`;
-                
-        }
-        
-        else if (playerSelection == computerSelection) {
-            playerScore, computerScore += 1;
-            return `Tie! You played ${playerSelection} and Computer also played ${computerSelection}! Score: ${playerScore} to ${computerScore}`;
-        }   
-    }
-
-    let game = () => {
-        for (let x = 0; x < 5; x++) {
-            console.log(playRound(prompt("Pick 'rock', 'paper' or 'scissors'"), computerPlay()))   
-    }
-
-        if (playerScore > computerScore) {
-            console.log("Congrats you beat the computer")
-        }
-
-        else if (playerScore < computerScore) {
-            console.log("Computer wins, better luck next time")
-        }
-
-        else if (playerScore === computerScore) {
-            console.log("Tie")
-        }
-    }
-    game();
+	view.displayResults('First to score 5 points wins!');
+	view.displayScore(playerScore, computerScore);
+	view.removeWinner();
+	view.removePlayAgain();
+};
